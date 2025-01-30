3caf8a959d57ede89b155c264adf8190519c7937
davitbek <davitzeynalyan1991@gmail.com>
2025-01-30 05:26:38 +0400

    add actions

---
+++ src/Actions/ItemActionList.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Vanacode\Model\Actions;
+
+use Illuminate\Support\Arr;
+use Vanacode\Model\Interfaces\ModelInterface;
+
+class ItemActionList extends ActionList
+{
+    public function __construct(protected readonly ModelInterface $item, string $resource, array $actions = [])
+    {
+        parent::__construct($resource, $actions);
+    }
+
+    protected function canDoAction(string $action, array $options): bool
+    {
+        return $this->item->canDoAction($action, $options);
+    }
+
+    protected function defaultActions(): array
+    {
+        $actions = $this->item->getActions();
+        $traitActions = $this->getTraitActions();
+        $allActions = array_merge([$actions], $traitActions);
+
+        return $this->mergeActions($allActions);
+    }
+
+    protected function makeAction(string $action, array $options): ItemAction
+    {
+        return new ItemAction($this->item, $action, $this->resource, $options);
+    }
+
+    protected function getActionOptions(): array
+    {
+        return config('vn_model.item_actions', []);
+    }
+
+    protected function getRouteDynamicParams(array &$options): array
+    {
+        $itemParameters = Arr::pull($options, 'item_parameters', []);
+        if (empty($itemParameters)) {
+            return [$this->item->getRouteKey()];
+        }
+        $routeParams = [];
+        foreach ($itemParameters as $parameter => $attribute) {
+            if (is_numeric($parameter)) {
+                $parameter = $attribute;
+            }
+            $routeParams[$parameter] = object_get($this->item, $attribute);
+        }
+
+        return $routeParams;
+    }
+
+    protected function getTraitActions(): array
+    {
+        if ($this->item->skipTraitAction) {
+            return [];
+        }
+
+        $methods = $this->item->getMethodsMatch('getActionsBy');
+        $actions = [];
+        foreach ($methods as $method) {
+            $actions[] = $this->item->$method();
+        }
+
+        return $actions;
+    }
+}
---
+++ src/Actions/StaticActionList.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Vanacode\Model\Actions;
+
+class StaticActionList extends ActionList
+{
+    public function __construct(protected readonly string $modelClass, string $resource, array $actions = [])
+    {
+        parent::__construct($resource, $actions);
+    }
+
+    protected function canDoAction(string $action, array $options): bool
+    {
+        return true;
+    }
+
+    protected function defaultActions(): array
+    {
+        $actions = $this->modelClass::getStaticActions();
+        $traitActions = $this->getTraitActions();
+        $allActions = array_merge($traitActions, [$actions]);
+
+        return $this->mergeActions($allActions);
+    }
+
+    protected function getTraitActions(): array
+    {
+        $methods = (new $this->modelClass)->getMethodsMatch('getStaticActionsBy');
+        $actions = [];
+        foreach ($methods as $method) {
+            $actions[] = $this->modelClass::$method();
+        }
+
+        return $actions;
+    }
+
+    protected function getRouteDynamicParams(array &$options): array
+    {
+        return [];
+    }
+
+    protected function makeAction(string $action, array $options): StaticAction
+    {
+        return new StaticAction($this->modelClass, $action, $this->resource, $options);
+    }
+
+    protected function getActionOptions(): array
+    {
+        return config('vn_model.static_actions', []);
+    }
+}
